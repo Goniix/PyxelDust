00fd26edfafac3961282c7be7d98c2b5af00b49f
goniix <87310572+Goniix@users.noreply.github.com>
2023-02-10 11:24:49 +0100

    removed vscode shit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pyxel
 class Game:
-    def __init__(self) -> None:
+    def __init__(self):
         pyxel.init(720,480)
         pyxel.run(self._update, self._draw)
         
-    def _update(self) -> None:
+    def _update(self):
         pass
 
-    def _draw(self) -> None:
+    def _draw(self):
         pass
 
 Game()
